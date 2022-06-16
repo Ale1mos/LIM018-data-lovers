3876560da4b3fc87b656149eef46a706cd1b50e4
Ale1mos <alejandramosqueirazapata1@gmail.com>
2022-06-16 11:52:39 -0500

    jalar data

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@
 // import data from './data/lol/lol.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
+console.log(data);
+// console.log(data.films[1].poster);
+// let imagen=document.getElementById("imagen")
+// let poster =data.films[1].poster
+// imagen.src=poster;
 
-// console.log(data);
 
 
 
-let texto =films.includes("poster")
-console.log(texto);
+// let texto =films.includes("poster")
+// console.log(texto);
