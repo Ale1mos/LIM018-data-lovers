6665d6c34d34831e95d7f1f57a33d1b43751bf3d
belen <anabelenarroyo21@gmail.com>
2022-06-25 00:34:57 -0500

    agregadas sentencias mostrar ordenar filtrar personajes

--- src/main.js
+++ src/main.js
@@ -36,6 +36,47 @@ filtrado_director.forEach(function(filtrado_director){
   imagen.innerHTML+= `<div>${filtrado_director.title} <img src='${filtrado_director.poster}'> </div>`
 })
 
+/// PERSONAJES /// 
+
+/// PERSONAJES EN PANTALLA
+films.forEach(function(films){
+  films.people.forEach(function(people){
+    console.log('personaje',people.name);
+    imagen.innerHTML+= `<div>${people.name} <img src='${people.img}'> </div>`
+  })
+})
+
+/// PERSONAJES ORDENADOS EN PANTALLA
+films.forEach(function(films){
+  films.people.sort((a,b) =>{
+    if(a.name < b.name){
+      return -1;
+    }
+    if(a.name > b.name){
+      return 1;
+    }
+    return 0;
+  })
+  console.log('personajes ordenado',films.people);
+  films.people.forEach(function(people){
+    console.log('personaje ordenado', people.name);
+    imagen.innerHTML+= `<div>${people.name} <img src='${people.img}'> </div>`
+  })
+})
+
+/// PERSONAJES FILTRADOS EN PANTALLA
+films.forEach(function(films){
+  const filtrado = films.people.filter(people => people.specie == "Cat");
+  console.log('personajes filtrados', filtrado);
+  filtrado.forEach(function(people){
+    console.log('personaje filtrado',people.name);
+    imagen.innerHTML+= `<div>${people.name} <img src='${people.img}'> </div>`
+  })
+})
+
+
+
+
 
 
 
