6222da932dfed721fc1bbc8b891b45db48938e84
Ale1mos <alejandramosqueirazapata1@gmail.com>
2022-07-09 22:35:38 -0500

    actualizacion

--- src/main.js
+++ src/main.js
@@ -128,37 +128,6 @@ btnAnimaciones.addEventListener("click",function(){
 
 
 
-// const especie = 'Cat'
-// let imagen=document.getElementById("imagen")
-
-
-/// PERSONAJES EN PANTALLA///
-// const personajes = obtenerPersonajes(films)
-// for (let i = 0 ; i< personajes.length ; i++){
-//     imagen.innerHTML+=`<div>${personajes[i].name} <img src='${personajes[i].img}'> </div>`;
-//   }
-// console.log('personajes mostrados')
-
-
-/// PERSONAJES ORDENADOS EN PANTALLA A-Z ///
-// no ordena aun
-// ordenarPersonajes(personajes)
-// for(let i=0 ; i< personajes.length; i++){
-//     imagen.innerHTML+=`<div>${personajes[i].name} <img src='${personajes[i].img}'> </div>`;
-//   }
-// console.log('personajes ordenados mostrados')
-
-
-/// PERSONAJES FILTRADOS EN PANTALLA ///
-// const personajes_filtrado = filtrarPersonajes(personajes,especie)
-// for(let i=0 ; i< personajes_filtrado.length; i++){
-//   imagen.innerHTML+=`<div>${personajes_filtrado[i].name} <img src='${personajes_filtrado[i].img}'> </div>`;
-// }
-// console.log('personajes filtrados mostrados')
-
-
-
-
 
 
 
